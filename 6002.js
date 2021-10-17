9cfb03f290e6353bd7a017bbf8865e67417ac901
Yao Lee <56864407+mrliyao@users.noreply.github.com>
2021-10-17 12:25:17 +0800

    Create 6002.js

---
+++ 6002.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "天津大学",
+    id: "6002",
+    link: [ {
+        name: "天大主页",
+        url: "http://www.tju.edu.cn/"
+    }, {
+        name: "教务网",
+        url: "http://classes.tju.edu.cn/"
+    }, {
+        name: "办公网",
+        url: "http://e.tju.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://www.lib.tju.edu.cn/"		
+    }, {
+        name: "天外天",
+        url: "https://www.twt.edu.cn/"		
+    }, {
+        name: "邮件系统",
+        url: "https://mail.tju.edu.cn/"
+    }, {
+        name: "北洋PT",
+        url: "https://www.tjupt.org/"
+    } ]
+};
